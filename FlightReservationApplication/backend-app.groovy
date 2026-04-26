c2a3917225435e803a871f8fac9b420aae07c75b
omryakawar <omryakawar@gmail.com>
2026-04-26 22:56:44 +0530

    updated sonar project key and docker image name in deployment.yaml

--- FlightReservationApplication/backend-app.groovy
+++ FlightReservationApplication/backend-app.groovy
@@ -23,7 +23,7 @@ pipeline {
                 withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
                  sh '''
                     cd FlightReservationApplication
-                    mvn sonar:sonar -Dsonar.projectKey=back
+                    mvn sonar:sonar -Dsonar.projectKey=backend
                  '''
                 }
             }
